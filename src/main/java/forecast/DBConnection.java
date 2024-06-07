176fddaba475aa5decd39b25e88a7693c16fad2b
LifeIsOne <didtmdgh258@gmail.com>
2024-06-07 21:13:57 +0900

    JDBC 생성 DB와 연결

---
+++ src/main/java/forecast/DBConnection.java
@@ -0,0 +1,21 @@
+package forecast;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+    public static Connection getConnection(){
+        String username = "root";
+        String password = "1234";
+        String url = "jdbc:mariadb://127.0.0.1:3306/forecast_db";
+
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            System.out.println("db connect success");
+            return connection;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
