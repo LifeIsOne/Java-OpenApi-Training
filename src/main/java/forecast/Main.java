01e3af13e0a029cd2bea9be0639fc2f59404d451
LifeIsOne <didtmdgh258@gmail.com>
2024-06-10 01:57:01 +0900

    메인 작성하기

--- src/main/java/forecast/Main.java
+++ src/main/java/forecast/Main.java
@@ -1,33 +1,52 @@
 package forecast;
 
 
-import forecast.location.LocationTable;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        LocationDAO dao = new LocationDAO(DBConnection.getConnection());
 
-        DBConnection db = new DBConnection();
+        System.out.println("다음 중 하나의 지역을 입력하세요!");
+
+        // 1단계 리스트 출력 ( 도, 시 )
+        List<String> levelOneList = dao.SearchLevelOne();
+        levelOneList.forEach(s -> System.out.print("["+s+"] "));
+        System.out.println();
+        System.out.print(" ↪ ");
+
+        // 1단계 입력
+        String levelOne = sc.nextLine();
+        System.out.println();
+
+        // 2단계 리스트 출력 ( 구, 군 )
+        System.out.println("다음 중 하나의 구 또는 군을 입력하세요!!");
+        List<String> levelTwoList = dao.SearchLevelTwo(levelOne);
+        levelTwoList.forEach(s -> System.out.print("["+s+"] "));
+        System.out.println();
+        System.out.print(" ↪ ");
+
+        // 2단계 입력
+        String levelTwo = sc.nextLine();
+        System.out.println();
+
+        // 3단계 리스트 출력 ( 동, 읍, 면 )
+        System.out.println("다음 중 하나의 동을 입력하세요!!");
+        List<String> levelThreeList = dao.SearchLevelThree(levelOne, levelTwo);
+        levelThreeList.forEach(s -> System.out.print("["+s+"] "));
+        System.out.println();
+        System.out.print(" ↪ ");
+
+        // 3단계 입력
+        String levelThree = sc.nextLine();
+        System.out.println();
 
 
 
-        // 입력하기
-        System.out.print("'시'를 입력해주세요 ( 예 부산광역시/...) : ");
-        String level1 = sc.nextLine();
-        System.out.print("'구'를 입력해주세요 ( 예 사상구/...) : ");
-        String level2 = sc.nextLine();
-        System.out.print("'동'를 입력해주세요 ( 예 괘법동/..) : ");
-        String level3 = sc.nextLine();
-
-
-
-//        LocationDAO locationDAO = new LocationDAO();
-
-
-        
         System.out.println("😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍");
 
     }
